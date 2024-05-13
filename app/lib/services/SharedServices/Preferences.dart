48dfb17f8709a282227c164b86fc7d9be41c18a4
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-05-13 21:04:17 +0530

    folder structure updated

--- app/lib/services/SharedServices/Preferences.dart
+++ app/lib/services/SharedServices/Preferences.dart
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 SharedPreferences? preferences;
-=======
-import 'package:shared_preferences/shared_preferences.dart';
 
-SharedPreferences? preferences;
->>>>>>> upstream/main
